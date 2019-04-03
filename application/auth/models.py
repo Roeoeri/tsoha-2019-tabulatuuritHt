1f7a9e4aa9c9b4c452dd245b2eb4dfd246666955
tomih <tomi.haapakoski@helsinki.fi>
2019-04-03 04:57:38 +0300

    Lisätty tabulatuuriin tieto sen tekijästä, sekä oma taulu genrelle ja liitostaulu genren ja kappaleen välille

--- application/auth/models.py
+++ application/auth/models.py
@@ -9,6 +9,8 @@ class User(db.Model):
     username = db.Column(db.String(144), nullable=False)
     password = db.Column(db.String(144), nullable=False)
 
+    tabs = db.relationship("Tab", backref='account', lazy =True)
+
     def __init__ (self,username,password):
         self.username = username
         self.password = password
--- application/tabs/models.py
+++ application/tabs/models.py
@@ -6,9 +6,30 @@ class Tab(db.Model):
 
 	name = db.Column(db.String(150), nullable=False)
 	content = db.Column(db.String(2000), nullable=False)
+
+	account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable = False)
+
 	
 	def __init__(self,name,content):
 		self.name = name
 		self.content = content
 
 
+class Genre(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	genre = db.Column(db.String(150), nullable=False)
+
+	genreTabs = db.relationship("GenreTab", backref='genre', lazy =True)
+
+	def __init__(self,genre):
+		self.genre = genre
+
+class GenreTab(db.Model):
+	genre_id = db.Column(db.Integer, db.ForeignKey('tab.id'), primary_key=True)
+	tab_id = db.Column(db.Integer, db.ForeignKey('genre.id'), primary_key=True)
+
+	def __init__(self,genre_id,tab_id):
+		self.genre_id = genre_id
+		self.tab_id = tab_id
+	
+		
