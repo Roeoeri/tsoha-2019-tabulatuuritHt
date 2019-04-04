6e7b62355733452446228f35d4b22309bdc67617
tomih <tomi.haapakoski@helsinki.fi>
2019-04-04 20:53:38 +0300

    korjattu viiteavaimet

--- application/genreTab/models.py
+++ application/genreTab/models.py
@@ -1,8 +1,8 @@
 from application import db
 
 class GenreTab(db.Model):
-	genre_id = db.Column(db.Integer, db.ForeignKey('tab.id'), primary_key=True)
-	tab_id = db.Column(db.Integer, db.ForeignKey('genre.id'), primary_key=True)
+	genre_id = db.Column(db.Integer, db.ForeignKey('genre.id'), primary_key=True)
+	tab_id = db.Column(db.Integer, db.ForeignKey('tab.id'), primary_key=True)
 
 	def __init__(self,genre_id,tab_id):
 		self.genre_id = genre_id
