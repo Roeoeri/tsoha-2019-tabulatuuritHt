43caae7677cc7ef709adc26fc3e0cee53cf55c2d
tomih <tomi.haapakoski@helsinki.fi>
2019-04-25 18:06:43 +0300

    yritetty korjata yhteenvetokysely, jottei se riko herokun tietokantaa

--- application/genres/models.py
+++ application/genres/models.py
@@ -7,12 +7,16 @@ class Genre(db.Model):
 	
 	@staticmethod
 	def find_tabs_in_genre():
-		stmt = "Select genre.genre, count(*), genre.id from genre_tab join genre on genre.id = genre_tab.genre_id group by genre.genre"
+		stmt = "Select genre.genre, count(*) from genre_tab join genre on genre.id = genre_tab.genre_id group by genre.genre"
 
 		res = db.engine.execute(stmt)
 		response = []
 		for row in res:
-			response.append({"genre": row[0], "amount": row[1], "id":row[2]})
+			genre = row[0]
+			amount = row[1]
+			genreId = Genre.query.filter_by(genre = genre).first().id
+
+			response.append({"genre": genre, "amount": amount, "id": genreId})
 		return response
 
 	def __init__(self,genre):
