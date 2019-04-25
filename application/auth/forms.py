c75951f211709be2faf5f222d9e404239464ad1d
tomih <tomi.haapakoski@helsinki.fi>
2019-04-25 21:24:12 +0300

    Tehty kirjautumisesta ja rekisteröirtymisestä saavutettavampaa label foreilla ja päivitetty niiden ulkoasua

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -4,15 +4,15 @@ from wtforms import PasswordField, StringField, validators
 
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", [validators.Length(min=1, message="Syötä käyttäjätunnus")])
-    password = PasswordField("Password", [validators.Length(min=1, message="Syötä salasana")])
+    username = StringField("Käyttäjätunnus", [validators.Length(min=1, message="Syötä käyttäjätunnus")])
+    password = PasswordField("Salasana", [validators.Length(min=1, message="Syötä salasana")])
 
     class Meta:
         csrf = False
 
 class RegisterForm(FlaskForm):
-    username = StringField("Username", [validators.Length(min=1, message="Syötä käyttäjätunnus")])
-    password = PasswordField("Password", [validators.Length(min=1, message="Syötä salasana")])
+    username = StringField("Käyttäjätunnus", [validators.Length(min=1, message="Syötä käyttäjätunnus")])
+    password = PasswordField("Salasana", [validators.Length(min=1, message="Syötä salasana")])
 
     class Meta:
         csrf = False
